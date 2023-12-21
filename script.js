299b0179f9a039d17d64bfa00f66ee4394d6d113
harysetiawan23 <setiawan230896@gmail.comm>
2023-12-21 15:38:33 +0800

    feat: initialize message components

---
+++ script.js
@@ -0,0 +1,7 @@
+const rpgCharacterPicker = {
+    jobs: ['Knight', 'Mage', 'Rogue', 'Cleric', 'Archer', 'Druid', 'Bard', 'Alchemist'],
+    races: ['Elf', 'Goblin', 'Human', 'Demi-Human',],
+    extraSkills: ['Stealth', 'Telekinesis', 'Shape-shifting', 'Elemental Control', 'Healing', 'Invisibility', 'Summoning'],
+    goals: ['Seek vengeance for a fallen comrade', 'Unlock the secrets of ancient magic', 'Become the greatest warrior in the land', 'Find a lost artifact and return it to its rightful place', 'Restore balance to the world', 'Discover the truth behind a mysterious prophecy']
+}
+
